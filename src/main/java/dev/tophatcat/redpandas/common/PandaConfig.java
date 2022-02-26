464bed80d9b8d3b6cd717fd14fbdeb28983a04aa
KiriCattus <kiri_cattus@icloud.com>
2022-02-26 01:58:34 +0000

    Format code and run the license task.

--- src/main/java/dev/tophatcat/redpandas/common/PandaConfig.java
+++ src/main/java/dev/tophatcat/redpandas/common/PandaConfig.java
@@ -1,3 +1,23 @@
+/*
+ * Red Pandas - https://tophatcat.dev/red-pandas
+ * Copyright (C) 2016-2022 <KiriCattus>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ */
 package dev.tophatcat.redpandas.common;
 
 import com.google.common.collect.Lists;
@@ -29,18 +49,18 @@ public class PandaConfig {
                 .define("enableNaturalSpawning", true);
 
             redPandaSpawnWeight = builder.comment("If -1, the default spawn weight will be used. "
-                + "(The higher the value the more will spawn)")
+                    + "(The higher the value the more will spawn)")
                 .defineInRange("spawnWeight", 4, -1, Integer.MAX_VALUE);
 
             biomeAllowList = builder.comment("If biomes are specified here,"
-                + " Red Pandas will ONLY spawn in these biomes. (The disallowList is ignored while this is set!)")
+                    + " Red Pandas will ONLY spawn in these biomes. (The disallowList is ignored while this is set!)")
                 .defineList("allowList", Lists.newArrayList(
                     "minecraft:bamboo_jungle",
                     "minecraft:bamboo_jungle_hills"
                 ), o -> o instanceof String);
 
             biomeDisallowList = builder.comment("If the allowlist is not used, use this list to specify the"
-                + " biomes that Red Pandas should not spawn in.")
+                    + " biomes that Red Pandas should not spawn in.")
                 .defineList("disallowList",
                     Lists.newArrayList(
                         "minecraft:the_void",
